d8c7f46bedcaab7bac62ab5eb9c214672e89ecca
Bawb <meancraft@hotmail.com>
2015-04-15 20:54:41 -0700

    PowerUp Start
    Set up for power ups

---
+++ TopDown/src/mygame/entity/powerup/PowerUp.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame.entity.powerup;
+
+/**
+ *
+ * @author Bawb
+ */
+public class PowerUp {
+    
+}
